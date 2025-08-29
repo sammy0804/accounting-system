46b4356f8f0fa70fd0fa887c0d317a6cad7e95ff
juan almanza <jalmanza@russ.com.co>
2025-08-28 23:11:44 -0500

    General colors

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,19 +4,19 @@ import { useState } from "react";
 
 const linkBase = "block px-3 py-2 rounded-lg transition";
 const active = "bg-gray-900 text-white";
-const inactive = "text-gray-700 hover:bg-gray-100";
+const inactive = "text-white hover:bg-gray-100 hover:text-black";
 
 
 export function Sidebar() {
     const [open, setOpen] = useState(true);
     return (
-        <aside className={`h-screen sticky top-0 border-r bg-white ${open ? "w-64" : "w-16"} transition-all`}>
+        <aside className={`h-screen sticky top-0 border-r bg-black ${open ? "w-64" : "w-16"} transition-all`}>
             <div className="flex items-center justify-between p-3">
                 <div className="flex items-center gap-2">
                     <img src="/public/assets/svg/softmaster_logo.svg" alt="Logo" className={`bg-black transition-all ${open ? "w-20" : "w-8"}`} />
-                    {open && <span className="text-sm text-gray-700 font-semibold">Accounting</span>}
+                    {/* {open && <span className="text-sm text-gray-700 font-semibold">Accounting</span>} */}
                 </div>
-                <button className="text-xl font-bold text-black ml-2" title={open ? "Cerrar" : "Abrir"} onClick={() => setOpen(!open)}>
+                <button className="text-xl font-bold text-white ml-2" title={open ? "Cerrar" : "Abrir"} onClick={() => setOpen(!open)}>
                     &#9776;
                 </button>
             </div>
--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,8 +1,8 @@
 export function Topbar() {
     return (
-        <header className="h-14 border-b bg-white flex items-center justify-between px-4">
-            <h1 className="font-semibold text-black">Sistema contable</h1>
-            <div className="text-sm text-gray-500">v0.1</div>
+        <header className="h-14 border-b bg-black flex items-center justify-between px-4">
+            <h1 className="font-semibold text-white">Sistema contable</h1>
+            <div className="text-sm text-white">v0.1</div>
         </header>
     );
 }
