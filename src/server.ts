f00aa093f128bacee34850636184fe89c540f008
sammy0804 <sami31rincon@gmail.com>
2025-09-26 22:35:28 -0500

    started to configure backend for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { PrismaClient } from "./generated/prisma";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
+
+// 👉 servir el frontend
+app.use(express.static(path.join(__dirname, "../dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
 // helper redondeo 2 decimales
 const r2 = (n: number) => Math.round((n + Number.EPSILON) * 100) / 100;
